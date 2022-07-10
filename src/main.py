1a0ba69203ecedd18860e4c8a222859087af5c4c
IsaraAomi <isara.aomi.87u@gmail.com>
2022-07-10 11:56:49 +0900

    Edited src files.

--- src/main.py
+++ src/main.py
@@ -3,16 +3,24 @@ from private_info import *
 from libs import *
 
 
+"""
+Setting
+"""
+SEARCH_END_TIME   = "2022-06-15T00:00:00Z"  # UTC
+SEARCH_START_TIME = "2022-06-10T00:00:00Z"  # UTC
+SEARCH_WORD       = "あなたのサークル"
+
+
 def main():
     """
     - Args:
     - Returns:
     """
     FollowingUsersList = GetUser_Following(TwitterMyAccountInfo.user_id)
-    FollowingUsersTweetsList = GetFollowingUsersTweets(FollowingUsersList, end_time="2022-06-15T00:00:00Z", start_time="2022-06-10T00:00:00Z", process="multi")
-    SearchedTweetsList = SearchTweetsInTweetsList(FollowingUsersTweetsList, word="あなたのサークル")
+    FollowingUsersTweetsList = GetFollowingUsersTweets(FollowingUsersList, end_time=SEARCH_END_TIME, start_time=SEARCH_START_TIME, process="multi")
+    SearchedTweetsList = SearchTweetsInTweetsList(FollowingUsersTweetsList, word=SEARCH_WORD)
     for i, SearchedTweet in enumerate(SearchedTweetsList):
-        print(i, SearchedTweet)
+        print_info("{} {}".format(i, SearchedTweet))
     SaveTweetsListAsCsv(SearchedTweetsList)
 
 
