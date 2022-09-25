055cffa35ca6a84460b7ca531c9a31504d46734d
IsaraAomi <isara.aomi.87u@gmail.com>
2022-09-26 02:34:14 +0900

    Updated codes.

--- src/word_cloud.py
+++ src/word_cloud.py
@@ -4,6 +4,10 @@ import pprint
 import MeCab
 from PIL import Image
 from wordcloud import WordCloud
+from tqdm import tqdm
+
+
+SHORT_PROGRESS_BAR="{l_bar}{bar:20}{r_bar}{bar:-10b}"
 
 
 def read_text_from_csv_to_list(file_path):
@@ -44,8 +48,8 @@ def create_word_cloud_from_csv(file_path):
     # exit()
 
     ## colormap: https://matplotlib.org/3.1.0/tutorials/colors/colormaps.html
-    width  = 1500
-    height = 500
+    width  = 3000
+    height = 1000
     mask_img = np.array(Image.open("../data/twitter_header_cat.png"))
     wc = WordCloud(
         width=width, height=height, background_color="white", colormap="viridis",
@@ -53,7 +57,7 @@ def create_word_cloud_from_csv(file_path):
         font_path="../fonts/SourceHanCodeJP-Medium.otf",
         stopwords={"みたい","そう","あと","こと","もの","これ","ため","それ","ところ","よう","co","https","http","RT","C1","そこ","ここ","感じ","さん","わけ","すぎ","どこ","あれ","とき","まま","しない","うち","なん","なんだろう","マジ","めちゃくちゃ","とりあえず","きた","だめ","しよう","以下","もん","わからん","自分","今回","今日","明日","先生","ダメ","amp","the","in","gt","on","t","の","ん","マジで","A","v","x","さ","ついで","K","O","0","B","C","人","的","化","W","次","先","R","E","s","版","Y","P","J","I","v","やつ","はず","かね","以上","ホント","アレ","問題","個人","普通","流石"},
     )
-    for i in range(10):
+    for i in tqdm(range(10), bar_format=SHORT_PROGRESS_BAR):
         wc.generate(" ".join(s))
         wc.to_file(f"../data/wordcloud_{width}x{height}_{i}.png")
 
